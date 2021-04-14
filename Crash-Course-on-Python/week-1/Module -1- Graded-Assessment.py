c1d9b5bec66ead1f7c907d3f4d1e72a052feda53
sorwarduet <esorwar.cse@gmail.com>
2021-04-14 23:23:33 +0600

    Add Python Course first 3 week code

---
+++ Crash-Course-on-Python/week-1/Module -1- Graded-Assessment.py
@@ -0,0 +1,19 @@
+"""
+Write a Python script that outputs "Automating with Python is fun!" to the screen.
+"""
+print("Automating with Python is fun!")
+
+"""
+Fill in the blanks so that the code prints "Yellow is the color of sunshine".
+"""
+color = "Yellow"
+thing = 'sunshine'
+print(color + " is the color of " + thing)
+
+"""
+Keeping in mind there are 86400 seconds per day, write a program that calculates how many seconds there are in a week, 
+if a week is 7 days.  Print the result on the screen.
+"""
+
+seconds = 7 * 86400
+print(seconds)
